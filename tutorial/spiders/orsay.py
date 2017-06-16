049ae2f8cccbe6188a1b58c1082299c848680135
Fakhar <fakhar.muhammad@arbisoft.com>
2017-06-16 22:21:05 +0500

    refined code

--- tutorial/spiders/orsay.py
+++ tutorial/spiders/orsay.py
@@ -1,5 +1,6 @@
 import scrapy
 import re
+import json
 
 
 class OrsaySpider(scrapy.Spider):
@@ -9,31 +10,46 @@ class OrsaySpider(scrapy.Spider):
         'http://www.orsay.com/de-de/shoulder-cut-out-top-10306897.html']
 
     def parse(self, response):
-        yield {
-            'brand': response.xpath(
-                "//script[@type='application/ld+json']/text()").re(r'Brand.*name":"(\w*)"},')[0],
-            'care': response.xpath("//div[@class='product-care six columns']//@src | //p[@class='material']/text()").extract(),
-            'description': response.xpath("//p[@class='description']/text()").extract(),
-            'image_urls': response.xpath("//div[@class='product-image-gallery-thumbs configurable']//@href").extract(),
-            'name': response.css("h1.product-name::text").extract(),
-            'retailer_sku': response.css("p.sku::text").re(r'(\d*)660000')[0],
-                'skus': {str(response.css("p.sku::text").re(r'(\d*)660000')[0] + '_' + response.css('div.sizebox-wrapper li[data-optionid="1151"]::text').extract()[0].strip()):
-                     {'color': response.css("img.has-tip[title]::attr(title)").extract()[0],
-                      'currency': response.xpath(
-                         "//script[@type='application/ld+json']/text()").re(r'priceCurrency\":\"(\w*)')[0], 'price': response.xpath(
-                         "//script[@type='application/ld+json']/text()").re(r'price":(\d*\.\d*)')[0], 'size': response.css('div.sizebox-wrapper li[data-optionid="1151"]::text').extract()[0].strip()}},
-            'size': list(filter(None, [n.strip() for n in response.css('div.sizebox-wrapper li::text').extract()])),
-            'test': response.xpath(
-                "//script[@type='application/ld+json']/text()").re(r'price":(\d*\.\d*)')[0]
+        f = open('orsay.json', 'w')
+        sizes = list(filter(None, [n.strip() for n in response.css(
+            'div.sizebox-wrapper li::text ').extract()]))
+        avail = response.xpath(
+            "//div[@class='sizebox-wrapper']//li/@data-qty").extract()
+        data = {
+            'brand': "",
+            'care': "",
+            'description': "",
+            'image_urls': "",
+            'name': "",
+            'retailer_sku': "",
+            'urls-color': "",
+            'skus': []
         }
+        data['brand'] = response.xpath(
+            "//script[@type='application/ld+json']/text()").re(r'Brand.*name":"(\w*)"},')[0]
+        data['care'] = response.xpath(
+            "//div[@class='product-care six columns']//@src | //p[@class='material']/text()").extract()
+        data['description'] = str(response.xpath(
+            "//p[@class='description']/text()").extract()[0]).strip()
+        data['image_urls'] = response.xpath(
+            "//div[@class='product-image-gallery-thumbs configurable']//@href").extract()
+        data['name'] = response.css("h1.product-name::text").extract()
+        data['url-colors'] = [n.strip() for n in response.xpath(
+            "//ul[@class='product-colors']//a/@href").extract() if n != '#']
+        data['retailer_sku'] = list(filter(None,response.css(
+            "p.sku::text").re(r'(\d*)')))[0]
+        for s in range(len(sizes)):
+            if int(avail[s]):
+                data['skus'].append({str(response.css("p.sku::text").re(r'(\d*)660000')[0] + '_' + sizes[s]):
+                                    {'color': response.css("img.has-tip[title]::attr(title)").extract()[0],
+                                     'currency': response.xpath(
+                                        "//script[@type='application/ld+json']/text()").re(r'priceCurrency\":\"(\w*)')[0], 'price': response.xpath(
+                                        "//script[@type='application/ld+json']/text()").re(r'price":(\d*\.\d*)')[0], 'size': sizes[s]}})
+            else:
+                data['skus'].append({str(response.css("p.sku::text").re(r'(\d*)660000')[0] + '_' + sizes[s]):
+                                    {'color': response.css("img.has-tip[title]::attr(title)").extract()[0],
+                                     'currency': response.xpath(
+                                        "//script[@type='application/ld+json']/text()").re(r'priceCurrency\":\"(\w*)')[0], 'price': response.xpath(
+                                        "//script[@type='application/ld+json']/text()").re(r'price":(\d*\.\d*)')[0], 'size': sizes[s], 'out_of_stock': 'true'}})
 
-    def parse_skus(self, response):
-        def extract_with_css(query):
-            return response.css(query)
-
-        yield {
-            'color': extract_with_css('h1.product-name::text'),
-            'currency': extract_with_css('h1.product-name::text'),
-            'price': extract_with_css('h1.product-name::text'),
-            'size': extract_with_css('h1.product-name::text'),
-        }
+        json.dump(data, f)
